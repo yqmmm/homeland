cc2ef922eee493c84c13b8aebbd79ce2753709d1
Qianmian Yu <im.qianmian.yu@gmail.com>
2020-05-16 09:38:39 +0800

    import sql

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,6 @@ require "mocha/minitest"
 require "rails/test_help"
 require "sidekiq/testing"
 
-require_relative "./sql_log"
 
 FileUtils.mkdir_p(Rails.root.join("tmp/cache"))
 
@@ -117,3 +116,63 @@ class ActionDispatch::IntegrationTest
     assert_match /\/account\/sign_in/, response.headers["Location"]
   end
 end
+
+# require_relative "./sql_log"
+
+module SqlSource
+  class << self
+    attr_accessor :current_tag
+    attr_accessor :dest, :bc
+
+    def find_source(trace)
+      return @bc.clean(trace)[-1]
+    end
+
+    def find_spec(trace)
+      index = trace.rindex { |x|
+        x.include? "_test.rb"
+      }
+      return nil if index.nil?
+      return trace[index].scan(/(?:.*\/)(.*?\w*?_test\.rb:[0-9]*)/)[0][0]
+    end
+
+    def register_sql(trace, sql)
+      source = self.find_source(trace)
+      spec = self.find_spec(trace)
+      return unless (not source.nil? and not sql.nil? and not spec.nil?)
+      tag = "#{source} !!! #{spec}"
+      if tag != @current_tag
+        if not @current_tag.nil?
+          @dest.puts("-#@current_tag")
+        end
+        @dest.puts("+#{tag}")
+        @current_tag = tag
+      end
+      @dest.puts("|>#{sql}")
+    end
+
+    def close()
+      return unless not @current_tag.nil?
+      @dest.puts("-#@current_tag")
+      @dest.close
+    end
+  end
+end
+
+SqlSource.dest = File.new(Rails.root.join("sql.logs"), "a")
+SqlSource.bc = ActiveSupport::BacktraceCleaner.new
+SqlSource.bc.add_filter { |line| line.gsub(Rails.root.to_s, '') }
+SqlSource.bc.add_silencer { |line| line =~ /\.rvm|_test/ }
+
+END {
+  SqlSource.close
+}
+
+ActiveSupport::Notifications.subscribe "sql.active_record" do |*args|
+  event = ActiveSupport::Notifications::Event.new *args
+
+  unless event.payload[:cached]
+    SqlSource.register_sql(caller, event.payload[:sql])
+  end
+end
+
